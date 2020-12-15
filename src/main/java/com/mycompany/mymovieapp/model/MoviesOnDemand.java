dc4c8b675ff0b932ff9e2cb4a9739d4566ce61fb
coffeeplant <56473451+coffeeplant@users.noreply.github.com>
2020-12-15 19:17:27 +0000

    Movies list working

--- src/main/java/com/mycompany/mymovieapp/model/MoviesOnDemand.java
+++ src/main/java/com/mycompany/mymovieapp/model/MoviesOnDemand.java
@@ -11,12 +11,15 @@ import java.util.List;
 import com.mycompany.mymovieapp.model.Movie;
 
 public class MoviesOnDemand {
-    List<Movie> allMovies = new ArrayList<Movie>();
-    allMovies.add(1, "Raiders of the Lost Ark", 1981, false, false, "1981 American action-adventure film directed by Steven Spielberg and written by Lawrence Kasdan based on a story by George Lucas and Philip Kaufman.", false);
-    allMovies.add(2, "The Other Guys", 2010, false, false, "2010 American buddy cop action comedy film directed by Adam McKay, who co-wrote it with Chris Henchy.", false)
-    allMovies.add(3, "Jurassic Park", 1993, false, false, "1993 American science fiction adventure film directed by Steven Spielberg and produced by Kathleen Kennedy and Gerald R. Molen.", false)
+
+    
+    public static void main(String[] args) {
+    List<Movie> allMovies = new ArrayList<>();
+    
+    allMovies.add(new Movie(1, "Raiders of the Lost Ark", 1981, false, false, "1981 American action-adventure film directed by Steven Spielberg and written by Lawrence Kasdan based on a story by George Lucas and Philip Kaufman.", false));
+    allMovies.add(new Movie(2, "The Other Guys", 2010, false, false, "2010 American buddy cop action comedy film directed by Adam McKay, who co-wrote it with Chris Henchy.", false));
+    allMovies.add(new Movie(3, "Jurassic Park", 1993, false, false, "1993 American science fiction adventure film directed by Steven Spielberg and produced by Kathleen Kennedy and Gerald R. Molen.", false));
     
     List<Customer> allCustomers = new ArrayList<>();
-    
-    
+    }
 }
